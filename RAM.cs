a07163cfed52bea070b370fb3912271063bb2aaf
Void-666 <77442200+Void-666@users.noreply.github.com>
2022-04-04 15:24:52 +0300

    It kills RAM and makes you restart your PC

---
+++ RAM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ozuTest
+{
+    class Program
+    {
+        static List<MyBuffer> buffer = new List<MyBuffer>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                MyBuffer buf = new MyBuffer();
+                buffer.Add(buf);
+                Console.WriteLine("Welcome to TON 618!");
+            }
+
+        }
+    }
+
+    class MyBuffer
+    {
+        byte[] buf = new byte[1048576];
+    }
+}
